b638aa3becd48ce7b0328dada13e9a677a30a871
agds-alt <agdscid@gmail.com>
2025-10-22 08:47:48 +0700

    start after location

--- src/presentation/hooks/index.tsx
+++
@@ -1,9 +0,0 @@
-// ============================================
-// 2. src/presentation/hooks/index.ts
-// ============================================
-export { useTimer } from './useTimer';
-export { usePhotoUpload } from './usePhotoUpload';
-export { useGeolocation } from './useGeolocation';
-export { useInspection } from './useInspection';
-export { useNotification } from './useNotification';
-export { useInspectionHistory } from './useInspectionHistory';
